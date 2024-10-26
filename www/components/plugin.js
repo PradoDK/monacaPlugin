91d41f9c1674e3ad178051d2e8d8e4167c331710
PradoDK <lucaspradoalves75@gmail.com>
2024-10-26 10:07:04 +0900

    hf

--- www/components/plugin.js
+++ www/components/plugin.js
@@ -1,5 +1,21 @@
 // This is a JavaScript file
 
 $(document).on("click", "#alerta", function(){
-    navigator.notification.alert("minha Mensagem");
+    navigator.notification.alert("O Iago eh baitola", retorno, "Aviso!", "Ok");
+});
+$(document).on("click", "#confirm", function(){
+    function confirma(buttonIndex){
+        if(buttonIndex == "1"){
+        navigator.notification.alert("Escolheu a opção A");
+        }else{
+        navigator.notification.alert("Escolheu a opção B");
+    }
+    }
+    navigator.notification.confirm("Escolha A ou B", confirma, "Escolha:",['A','B']);
+});
+$(document).on("click", "#beep", function(){
+    navigator.notification.beep(4);
+});
+$(document).on("click", "#vibration", function(){
+    navigator.vibrate(3000);
 });
